9a5046bf746323ea04b69804831e7bf9bb0641df
Igor Serganov <caesarandroidru@gmail.com>
2019-04-17 14:15:15 +0300

    refactoring

--- RearrangeArray/Program.cs
+++ RearrangeArray/Program.cs
@@ -42,19 +42,5 @@ namespace RearrangeArray
             }
         }
     }
-
-    public static class ArrayHelper
-    {
-        public static List<T> Rearrange<T>(this List<T> array, int divisionPoint)
-        {
-            if (divisionPoint < 0)
-                throw new Exception("incorrect index supplied");
-            if (divisionPoint == 0)
-                return array;
-            var result = array.Skip(divisionPoint).Take(array.Count() - divisionPoint).ToList();
-            result.AddRange(array.Take(divisionPoint));
-            return result;
-        }
-    }
 }
 
